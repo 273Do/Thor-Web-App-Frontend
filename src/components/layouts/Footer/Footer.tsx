7a9ce21e64061947e345760f5145902d1367c9b6
273* <keita.y310@icloud.com>
2024-11-29 23:36:58 +0900

    ✨ ヘッダーとフッターを実装しました．

---
+++ src/components/layouts/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 z-50 w-full border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-center">
+        <p>273DoWorks</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
