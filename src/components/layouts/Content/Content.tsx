f328f2f45c799a8199b54b50021fcf6d79fd9080
273* <keita.y310@icloud.com>
2024-12-01 18:34:06 +0900

    ✨ 下準備のコンテンツを実装しました．

--- src/components/layouts/Content/Content.tsx
+++ src/components/layouts/Content/Content.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Content = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <div className="h-[2px] w-full bg-muted"></div>
+      <div className="my-8">{children}</div>
+    </>
+  );
 };
 
 export default Content;
--- src/pages/LP.tsx
+++ src/pages/LP.tsx
@@ -1,11 +1,13 @@
 import * as Layout from "@/components/layouts/index.tsx";
+import { useTheme } from "@/components/theme-provider";
 import { Button } from "@/components/ui/button";
 
 const LP = () => {
+  const { theme } = useTheme();
   return (
     <>
       <Layout.Title title="アクティビティデータから睡眠状態を解析します。">
-        <div className="my-4 flex flex-col items-center text-muted-foreground">
+        <div className="my-8 flex flex-col items-center text-muted-foreground">
           <p>
             Thorはスマートフォンで計測されたアクティビティデータのみを用いて、
           </p>
@@ -16,7 +18,27 @@ const LP = () => {
         </div>
       </Layout.Title>
       <Layout.Content>
-        <div>内容</div>
+        <div>
+          <p className="mb-2 text-center text-xl font-bold">下準備</p>
+          <div className="flex gap-5">
+            <div className="text-center">
+              <p className="my-1">Step1：ヘルスケアアプリを開く</p>
+              <img
+                src={`imgs/step1${theme === "dark" ? "_dark" : ""}.png`}
+                className="w-72"
+                alt="step1"
+              />
+            </div>
+            <div className="text-center">
+              <p className="my-1">Step2：データをエクスポートする</p>
+              <img
+                src={`imgs/step2${theme === "dark" ? "_dark" : ""}.png`}
+                className="w-72"
+                alt="step1"
+              />
+            </div>
+          </div>
+        </div>
       </Layout.Content>
     </>
   );
