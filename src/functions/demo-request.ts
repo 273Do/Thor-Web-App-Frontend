db0e7cef9260f2359dcf61ad6d18148876e3dbe4
273* <keita.y310@icloud.com>
2024-12-16 22:55:16 +0900

    🔥 不要なコードを無効化してビルドできるようにしました．

--- src/functions/demo-request.ts
+++ src/functions/demo-request.ts
@@ -1,30 +1,30 @@
-let status = "pending";
-let result: any;
+// let status = "pending";
+// let result: any;
 
-// DEBUG: ダミーデータを取得するデバッグ用の関数
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// // DEBUG: ダミーデータを取得するデバッグ用の関数
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export function fetchPosts() {
-  const data = "../../../public/test_data/demo.json";
-  const fetching = fetch(data)
-    .then((res) => res.json())
-    .then(async (success) => {
-      await delay(4000);
-      status = "fulfilled";
-      result = success;
-    })
-    .catch((error) => {
-      status = "rejected";
-      result = error;
-    });
+// export function fetchPosts() {
+//   const data = "../../../public/test_data/demo.json";
+//   const fetching = fetch(data)
+//     .then((res) => res.json())
+//     .then(async (success) => {
+//       await delay(4000);
+//       status = "fulfilled";
+//       result = success;
+//     })
+//     .catch((error) => {
+//       status = "rejected";
+//       result = error;
+//     });
 
-  return () => {
-    if (status === "pending") {
-      throw fetching; // Promise を throw
-    } else if (status === "rejected") {
-      throw result;
-    } else if (status === "fulfilled") {
-      return result; // Promise が解決したら、取得したデータを返す
-    }
-  };
-}
+//   return () => {
+//     if (status === "pending") {
+//       throw fetching; // Promise を throw
+//     } else if (status === "rejected") {
+//       throw result;
+//     } else if (status === "fulfilled") {
+//       return result; // Promise が解決したら、取得したデータを返す
+//     }
+//   };
+// }
